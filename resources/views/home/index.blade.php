37ec111add2b4d960d6e75e4e2463a881d5f0cea
LohanDebner <lohan.debner@gmail.com>
2018-11-28 23:22:48 -0200

    Teste de integração King host 4

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,7 +12,7 @@
                             </h1>
                             <h1>
                                 ACRESCENTI PLATAFORMA
-                                Teste GitHub > King Host 3
+                                Teste GitHub > King Host 4
                             </h1>
                             <small>
                                 It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
